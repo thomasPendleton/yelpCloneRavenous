5ccbc9ea3f23571439bcbf2e7eb16fbcb9881a73
Thomas Pendleton <thomas6576@yahoo.com>
2022-05-17 19:11:51 -0700

    added api call to yelp to fetch business data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,31 +3,26 @@ import React from 'react'
 import './App.css'
 import BusinessList from '../BusinessList/BusinessList'
 import SearchBar from '../SearchBar/SearchBar'
-
-const business = {
-  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90,
-}
-
-const businesses = [business, business, business, business, business, business]
+import Yelp from '../../util/Yelp'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { business: [] }
+    this.searchYelp = this.searchYelp.bind(this)
+  }
   searchYelp(term, location, sortBy) {
-    console.log(`Searching yelp for ${term},${location},${sortBy}`)
+    Yelp.search(term, location, sortBy).then((businesses) =>
+      this.setState({ business: businesses })
+    )
   }
   render() {
+    console.log(this.state.business)
     return (
       <div className="App">
-        <h1>ravenous</h1>
+        <h1>Ravenous</h1>
         <SearchBar searchYelp={this.searchYelp} />
-        <BusinessList businesses={businesses} />
+        <BusinessList businesses={this.state.business} />
       </div>
     )
   }
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,12 +2,13 @@ import React from 'react'
 import './BusinessList.style.css'
 import Business from '../Business/Business'
 
+
 class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
         {this.props.businesses.map((business) => {
-          return <Business business={business} />
+          return <Business key={business.id} business={business} />
         })}
       </div>
     )
