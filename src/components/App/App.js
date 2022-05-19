7c5972882f302fee9bac9e9cd138d578c85df839
Thomas Pendleton <thomas6576@yahoo.com>
2022-05-18 21:53:16 -0700

    added notes for future updates

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,11 @@
-import React from 'react'
+//Make addresses clickable and have them open the address in Google Maps in a new tab
+//Clicking on a different sorting option automatically requeries the Yelp API, rather than having to manually click “Let’s Go” again
+//Implement your own type of sort (for example, by entering a distance or radius from a central location)
+//Allow you to search by pressing “Enter” (or “Return”) on your keyboard, as opposed to manually clicking
+//Add autocompletion of addresses to the “Location” input
+//Make images clickable and have them open the business’ website in a new tab
 
+import React from 'react'
 import './App.css'
 import BusinessList from '../BusinessList/BusinessList'
 import SearchBar from '../SearchBar/SearchBar'
